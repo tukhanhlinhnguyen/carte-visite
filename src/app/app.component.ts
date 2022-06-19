327e4b5dada154af64ed7701d14b0bc92b86968e
NGUYEN TU KHANH LINH <ntklsideproject@gmail.com>
2022-06-20 00:44:45 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'carte-visite';
+  name = 'Claudia Chalandon'
+  title = 'Consultant en référencement naturel';
+  font = 'JosefinSans-Thin'
+  tel = '06 95 89 68 66'
+  email = 'c.chalandon.seeubetter@gmail.com'
+  imageName = 'fed81db8-ef47-11ec-8ea0-0242ac120002.jpg'
+  image = `assets/img/${this.imageName}`
+  website = 'https://see-u-better.com/'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
